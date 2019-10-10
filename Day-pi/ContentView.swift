4ca230a8c5f67952c30d8fd4d8d93c70bdc57ecf
Edgardo Agno <edgardo.agno@gmail.com>
2019-10-10 12:19:16 +0100

    Initial Commit

---
+++ Day-pi/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Day-pi
+//
+//  Created by EDGARDO AGNO on 10/10/2019.
+//  Copyright © 2019 EDGARDO AGNO. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
